c2148fb15ca9af09f709a7210b4973127a52eddf
smouheb <smael.mouheb@gmail.com>
2017-09-18 06:25:01 +0200

    First iteration for issues #1 and #2

--- P6/app/AppKernel.php
+++ P6/app/AppKernel.php
@@ -15,7 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new OC\PrepBundle\OCPrepBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
